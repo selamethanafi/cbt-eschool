2119b7b5974d9cd5f87404f9aec576415e61fa1b
gludug <betaragludugbanyumurti@gmail.com>
2025-08-27 11:47:33 +0700

    Update pass.php

--- admin/pass.php
+++ admin/pass.php
@@ -87,7 +87,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Update Profil</title>
     <?php include '../inc/css.php'; ?>
 </head>
